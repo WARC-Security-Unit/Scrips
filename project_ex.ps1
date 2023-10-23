d2c649f18d66618937bfc05f6753e10609b6c330
Crist616 <144681232+Crist616@users.noreply.github.com>
2023-10-23 11:03:40 +0100

    Add files via upload

---
+++ project_ex.ps1
@@ -0,0 +1,37 @@
+# Install AD DS role 
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# Promote server to DC and create new forest
+$domainName = "SunToWater.com"
+$safeModeAdminPassword = ConvertTo-SecureString "P@ssw0rd1" -AsPlainText -Force
+Install-ADDSForest `
+  -DomainName $domainName `
+  -DomainNetbiosName "SUNTOWATER" `
+  -ForestMode "WinThreshold" ` 
+  -DomainMode "WinThreshold"
+  -InstallDns:$true `
+  -SafeModeAdministratorPassword $safeModeAdminPassword `
+  -Force:$true
+
+# Configure DNS server
+$ipAddress = "10.0.0.1"  
+$nic = Get-NetAdapter | Where-Object {$_.Status -eq "Up"}
+$dnsServer = Get-DnsServer
+$dnsServer.NetworkInterfaces[$nic.Name].IPv4Addresses = $ipAddress
+$dnsServer.Zones[0].DynamicUpdate = "Secure"
+
+# Create OU
+New-ADOrganizationalUnit `
+  -Name "Sales" `
+  -Path "DC=$domainName" 
+
+# Create user
+$username = "jdoe"
+$password = ConvertTo-SecureString "Password1" -AsPlainText -Force
+New-ADUser `
+  -Name "John Doe"
+  -GivenName "John"
+  -Surname "Doe" 
+  -SamAccountName $username 
+  -AccountPassword $password
+  -Enabled $true
