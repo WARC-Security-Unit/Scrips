c178f9ba7a2f3256a793b5b2b90ca004cd93f176
andrefsg <144686298+andrefsg@users.noreply.github.com>
2023-11-02 18:56:07 +0000

    warc_script.ps1

--- warc_script.ps1
+++ warc_script.ps1
@@ -23,40 +23,70 @@ Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 # Create an Active Directory Forest
 Install-ADDSForest -DomainName $NewDomainName -DomainMode Win2016 -ForestMode Win2016 -InstallDns -SafeModeAdministratorPassword $AdminPassword
 
-# Create Organizational Units (OU)
-New-ADOrganizationalUnit -Name "Sales" -Path "DC=$NewDomainName,DC=com"
-New-ADOrganizationalUnit -Name "IT" -Path "DC=$NewDomainName,DC=com"
+#Creating OUs and Users
+# Define OUs
+$OUs = @(
+    "Marketing",
+    "Financial",
+    "HR",
+    "Information",
+    "Information Security",
+    "Operations"
+)
 
-# Create users
-New-ADUser -Name "John Doe" -SamAccountName "johnd" -UserPrincipalName "johnd@$NewDomainName" -Enabled $true -Path "OU=Sales,DC=$NewDomainName,DC=com" -AccountPassword $AdminPassword
-New-ADUser -Name "Jane Smith" -SamAccountName "janes" -UserPrincipalName "janes@$NewDomainName" -Enabled $true -Path "OU=IT,DC=$NewDomainName,DC=com" -AccountPassword $AdminPassword
+# Create OUs
+foreach ($OU in $OUs) {
+    New-ADOrganizationalUnit -Name $OU -Path "DC=$DomainName"
+}
 
-# Assign a static IPv4 address to the Windows Server VM
-New-NetIPAddress -IPAddress $StaticIPAddress -PrefixLength 24 -InterfaceAlias "Ethernet" -DefaultGateway $Gateway
+# Create the user list
+$Users = @(
+    ("Bill", "Lumbergh", "Bill", "CEO", "Marketing"),
+    ("Peter", "Gibbons", "Peter", "Employee", "Marketing"),
+    ("Ron", "Weasley", "Ron", "Employee", "Marketing"),
+    ("Herry", "Potter", "Herry", "Employee", "Marketing"),
+    ("Hermione", "Granger", "Hermione", "Employee", "Financial"),
+    ("Bob", "Slydell", "Bob", "Employee", "Financial"),
+    ("Luca", "Pacioli", "Luca", "Employee", "Financial"),
+    ("Angela", "Martin", "Angela", "Employee", "Financial"),
+    ("Kevin", "Malone", "Kevin", "Employee", "Financial"),
+    ("Oscar", "Martinez", "Oscar", "Employee", "HR"),
+    ("Minerva", "McGonagall", "Minerva", "Employee", "HR"),
+    ("Sybill", "Trelawney", "Sybill", "Employee", "HR"),
+    ("Remus", "Lupin", "Remus", "Employee", "HR"),
+    ("Severus", "Snape", "Severus", "Employee", "Information"),
+    ("Bade", "Habib", "Bade", "Employee", "Information"),
+    ("Samir", "Nagheenanajar", "Samir", "Employee", "Information"),
+    ("Jhon", "Dorian", "Jhon", "Employee", "Information"),
+    ("Elliot", "Reid", "Elliot", "Employee", "Information"),
+    ("Chris", "Turk", "Chris", "Employee", "Information Security"),
+    ("Courtney", "Hans", "Courtney", "Employee", "Information Security"),
+    ("Brian", "Krebs", "Brian", "Employee", "Information Security"),
+    ("Milton", "Waddams", "Milton", "Employee", "Information Security"),
+    ("Perry", "Cox", "Perry", "Employee", "Information Security"),
+    ("Bob", "Kelso", "Bob", "Employee", "Operations"),
+    ("Michael", "Scott", "Michael", "Employee", "Operations"),
+    ("Dwight", "Schrute", "Dwight", "Employee", "Operations"),
+    ("Jim", "Halpert", "Jim", "Employee", "Operations"),
+    ("Andy", "Bernard", "Andy", "Employee", "Operations"),
+    ("Kelly", "Kapoor", "Kelly", "Employee", "Operations"),
+    ("Phyllis", "Lapin-Vance", "Phyllis", "Employee", "Operations"),
+    ("Meredith", "Palmer", "Meredith", "Employee", "Operations")
+)
 
-# Set DNS server address
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DNS
-
-# Rename the Windows Server VM
-Rename-Computer -NewName $ServerName -Restart
-
-# Install Active Directory Domain Services
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-
-# Create an Active Directory Forest
-Install-ADDSForest -DomainName $NewDomainName -DomainMode Win2016 -ForestMode Win2016 -InstallDns -SafeModeAdministratorPassword $AdminPassword
-
-# Create Organizational Units (OU)
-New-ADOrganizationalUnit -Name "Sales" -Path "DC=$NewDomainName,DC=com"
-New-ADOrganizationalUnit -Name "IT" -Path "DC=$NewDomainName,DC=com"
-
-# Import user data from CSV and create users
-$UserList = Import-Csv $CSVFile
-foreach ($User in $UserList) {
-    $FirstName = $User."Name"
-    #$LastName = $User."Sobrenome"
-    $Username = $User."Name"
-    $Password = ConvertTo-SecureString -AsPlainText $User."password" -Force
-    $Department = $User."Departamento"
+# Create users in the corresponding OUs
+foreach ($User in $Users) {
+    $FirstName, $LastName, $Username, $Title, $OUName = $User
+    $OUPath = "OU=$OUName,DC=$DomainName"
+    $UserParameters = @{
+        Name = "$FirstName $LastName"
+        SamAccountName = $Username
+        GivenName = $FirstName
+        Surname = $LastName
+        Title = $Title
+        Path = "OU=$OUName,DC=$DomainName"
     }
-    New-ADUser -Name "$FirstName" -SamAccountName $Username -UserPrincipalName "$Username@$NewDomainName" -Enabled $true -Path "OU=$Department,DC=$NewDomainName,DC=com" -AccountPassword $Password
+    New-ADUser @UserParameters
+}
+
+Write-Host "Creation of OUs and users successfully completed."
